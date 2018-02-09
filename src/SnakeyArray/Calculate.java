61133c092dc036f38ff0657bc93500d2926988f8
Sergey <gray4747@mail.ru>
2018-02-09 16:26:14 +0300

    Snakey filler.

--- src/SnakeyArray/Calculate.java
+++ src/SnakeyArray/Calculate.java
@@ -3,17 +3,18 @@ package SnakeyArray;
 public class Calculate {
     private enum Direction {LEFT, RIGHT, UP, DOWN}
 
-    public static void makeClockwise(int[][] array) {
-        if (array == null)
-            return;
+    public static int[][] makeClockwise(int height, int width) {
+        if (height < 1 || width < 1)
+            return null;
+        int[][] array = new int[height][width];
         int leftEdge = 0,
-                rightEdge = array[0].length - 1,
+                rightEdge = width - 1,
                 topEdge = 0,
-                bottomEdge = array.length - 1,
+                bottomEdge = height - 1,
                 x = 0, y = 0;
         Direction direction = Direction.RIGHT;
 
-        for (int i = 1; i <= array.length * array[0].length; i++) {
+        for (int i = 1; i <= height * width; i++) {
             array[y][x] = i;
             switch (direction) {
                 case UP:
@@ -21,8 +22,7 @@ public class Calculate {
                         direction = Direction.RIGHT;
                         x++;
                         leftEdge++;
-                    }
-                    else {
+                    } else {
                         y--;
                     }
                     break;
@@ -31,8 +31,7 @@ public class Calculate {
                         direction = Direction.LEFT;
                         x--;
                         rightEdge--;
-                    }
-                    else {
+                    } else {
                         y++;
                     }
                     break;
@@ -41,8 +40,7 @@ public class Calculate {
                         direction = Direction.UP;
                         y--;
                         bottomEdge--;
-                    }
-                    else {
+                    } else {
                         x--;
                     }
                     break;
@@ -51,12 +49,67 @@ public class Calculate {
                         direction = Direction.DOWN;
                         y++;
                         topEdge++;
-                    }
-                    else {
+                    } else {
                         x++;
                     }
                     break;
             }
         }
+        return array;
+    }
+
+    public static int[][] makeUnclockwise(int height, int width) {
+        if (height < 1 || width < 1)
+            return null;
+        int[][] array = new int[height][width];
+        int leftEdge = 0,
+                rightEdge = width - 1,
+                topEdge = 0,
+                bottomEdge = height - 1,
+                x = width - 1, y = 0;
+        Direction direction = Direction.LEFT;
+
+        for (int i = 1; i <= height * width; i++) {
+            array[y][x] = i;
+            switch (direction) {
+                case UP:
+                    if (y == topEdge) {
+                        direction = Direction.LEFT;
+                        x--;
+                        rightEdge--;
+                    } else {
+                        y--;
+                    }
+                    break;
+                case DOWN:
+                    if (y == bottomEdge) {
+                        direction = Direction.RIGHT;
+                        x++;
+                        leftEdge++;
+                    } else {
+                        y++;
+                    }
+                    break;
+                case LEFT:
+                    if (x == leftEdge) {
+                        direction = Direction.DOWN;
+                        y++;
+                        topEdge++;
+                    } else {
+                        x--;
+                    }
+                    break;
+                case RIGHT:
+                    if (x == rightEdge) {
+                        direction = Direction.UP;
+                        y--;
+                        bottomEdge--;
+                    } else {
+                        x++;
+                    }
+                    break;
+            }
+        }
+        return array;
     }
 }
--- src/SnakeyArray/Main.java
+++ src/SnakeyArray/Main.java
@@ -2,13 +2,12 @@ package SnakeyArray;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] array = new int[9][5];
-        Calculate.makeClockwise(array);
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                System.out.print(array[i][j]+"\u0009");
-        }
-        System.out.println();
+        int[][] array = Calculate.makeUnclockwise(9, 5);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\u0009");
+            }
+            System.out.println();
         }
     }
 }
